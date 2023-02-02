955aadf46c231cedf3db7eaf74fe93f160045cd3
jawad naimi <jdnaimi@fake.com>
2023-02-01 16:26:54 -0800

    added to cart

--- js/data-processing.js
+++ js/data-processing.js
@@ -7,11 +7,41 @@ if(queryString.length > 0){
     const urlParams = new URLSearchParams(queryString);
 
     let myData = "";
+    let myCart = "";
+    let myTotal = 0;//will store total cost
 
+    /* 
+    Widget: $3.99
+    Sprocket: $5.99
+    Thingy: $1.99
+
+    Total: $11.97
+    */
+
+    myCart += "<h3>Cart Contents</h3>";
     urlParams.forEach(function(value, key) {
         
       if(key == "Cart"){//cart
-        alert("Cart Item: " + value);
+        //alert("Cart Item: " + value);
+
+        switch(value){
+          case "widget":
+            myCart += "Widget: $3.99<br>";
+            myTotal += 3.99;
+            break;
+
+          case "Sprocket":
+            myCart += "Sprocket: $5.99<br>";
+            myTotal += 5.99;
+            break;
+
+          case "Thingy":
+            myCart += "Thingy: $1.99<br>";
+            myTotal += 1.99;
+            break;
+        }
+
+        
 
       }else{//shipping label 
         key = key.split("_").join(" ");
@@ -25,6 +55,11 @@ if(queryString.length > 0){
       //console.log(key, value);
     });
 
+    myCart += "Total: " + myTotal + '<br>';
+    
+    //we want the cart data first, then the shipping
+
+    myData = myCart + myData;
     myData += '<p><a href="index.html">CLEAR</a></p>';
 
     document.getElementById("output").innerHTML = myData;
