fb5ac66e558f3ecae8c18dac972ca419e5e05ab9
Naimi <naimij@amazon.com>
2023-03-21 18:47:55 -0700

    debugged

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,66 +1,46 @@
-//alert("hello from data-processing!")
-
+// Get query string
 const queryString = window.location.search;
 console.log(queryString);
 
-if(queryString.length > 0){
-    const urlParams = new URLSearchParams(queryString);
+// Parse query parameters
+if (queryString.length > 0) {
+  const urlParams = new URLSearchParams(queryString);
 
-    let myData = "";
-    let myCart = "";
-    let myTotal = 0;//will store total cost
+  // Initialize variables
+  let cartItems = [];
+  let totalCost = 0;
 
-    /* 
-    Widget: $3.99
-    Sprocket: $5.99
-    Thingy: $1.99
+  // Map of cart items and prices
+  const itemPrices = {
+    widget: 3.99,
+    sprocket: 5.99,
+    thingy: 1.99,
+  };
 
-    Total: $11.97
-    */
-
-    myCart += "<h3>Cart Contents</h3>";
-    urlParams.forEach(function(value, key) {
-        
-      if(key == "Cart"){//cart
-        //alert("Cart Item: " + value);
-
-        switch(value){
-          case "widget":
-            myCart += "Widget: $3.99<br>";
-            myTotal += 3.99;
-            break;
-
-          case "Sprocket":
-            myCart += "Sprocket: $5.99<br>";
-            myTotal += 5.99;
-            break;
-
-          case "Thingy":
-            myCart += "Thingy: $1.99<br>";
-            myTotal += 1.99;
-            break;
-        }
-
-        
-
-      }else{//shipping label 
-        key = key.split("_").join(" ");
-
-      }
-      
-      //swaps underscore for space 
-        key = key.split("_").join(" ");
-
-        myData += `<p>${key}: ${value}</p>`;
-      //console.log(key, value);
-    });
-
-    myCart += "Total: " + myTotal + '<br>';
-    
-    //we want the cart data first, then the shipping
-
-    myData = myCart + myData;
-    myData += '<p><a href="index.html">CLEAR</a></p>';
-
-    document.getElementById("output").innerHTML = myData;
+  // Process each query parameter
+  urlParams.forEach(function(value, key) {
+    // If cart item, add to cart and calculate total cost
+    if (key.toLowerCase() === 'cart') {
+      const itemPrice = itemPrices[value.toLowerCase()] || 0;
+      cartItems.push(`${value}: $${itemPrice.toFixed(2)}`);
+      totalCost += itemPrice;
+    } else {
+      // Replace underscores with spaces
+      key = key.split('_').join(' ');
+      cartItems.push(`${key}: ${value}`);
     }
+  });
+
+  // Format cart contents
+  let cartContents = `<h3>Cart Contents</h3>`;
+  if (cartItems.length > 0) {
+    cartContents += cartItems.join('<br>');
+    cartContents += `<br>Total: $${totalCost.toFixed(2)}<br>`;
+    cartContents += `<p><a href="index.html">CLEAR</a></p>`;
+  } else {
+    cartContents += `Cart is empty`;
+  }
+
+  // Display cart contents
+  document.getElementById('output').innerHTML = cartContents;
+}
